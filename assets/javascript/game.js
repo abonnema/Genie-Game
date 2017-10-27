20fad717e1eae6bbaaec4a570654114208397037
Thomas <thomasbonnema@andrews-air.attlocal.net>
2017-10-27 08:43:50 -0700

    second push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,96 +3,71 @@
     var amountLives = 10;
     var amountWin = 0;
     var amountLoss = 0;
+    
 
 
 // ARRAY VAR
     var listLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var letterIndex = 0;
-    var wordIndex = 0;
-    var wordList = ["avacado","melon","strawberry","apple","raspberries","passionfruit"];
-    var wordPicked = "";
+    var letterGuess = null;
+    var guessLetter = [];
+    var guessLeftLetter = null;
+    
 
-// ARRAY VAR FOR PLAYER CHOICE
-    //list of letters in word
-    var letterCount = [];
-    //how many underscores are needed
-    var blankHold = 0;
-    //holds amount of underscores
-    var letterUnderscore = [];
-    //holds correct guesses
-    var letterUnderscoreGuess = [];
-    //holds wrong letter chosen
-    var wrongLetter = [];
-
-
+    //Grab random letter
+    var computerGuess = listLetters[Math.floor(Math.random() * listLetters.length)];
+    console.log(computerGuess)
 
 // FUNCTION
     //Function to start the game
-    function startGame() {
-        //random choice of word
-        var wordPicked = wordList[Math.floor(Math.random() * wordList.length) + 1];
-        //split word into letters ???? look up
-        var amountLetters = wordPicked.split('');
-        //turn word into '_' underscore
-        var blankHold = amountLetters.length;
-        //counter amount
-        var amountLives = 10;
-        var letterCount = [];
-        var blankHold = 0;
-        var letterUnderscore = [];
-        var letterUnderscoreGuess = [];
-        var wrongLetter = [];
+
+
+    var updateLetter = function () {
+        this.letterGuess = this.listLetters[Math.floor(Math.random() * this.listLetters.length)];
+    };
+    var resetGame = function () {
+        amountLives = 10;
+        updateLetter();
+    };
+    
+    updateLetter();
 
     
-    //HTML  updates
-        document.getElementById('numberOfLives').innerHTML = amountLives;
-        document.getElementById('wordHangman').innerHTML = letterUnderscoreGuess.join(' ');
-        document.getElementById('winCounter').innerHTML = amountWin;
-        document.getElementById('lossCounter').innerHTML = amountLoss;
-        document.getElementById('wrongGuesses').innerHTML = wrongLetters;                      
-    //fill 
-    for(var i = 0; i < letterUnderscore; i++)
-        {
-            letterUnderscoreGuess.push('_');
-            document.getElementById('wordToGuess').innerHTML = blanksAndSuccesses;
-        }
-
-
- 
-    }
-    //Function to restart the game
-    function restartGame() {
-        
-            }
-    //Function to choose letter
-
-    function chooseLetter () {
-
-    }
-    //Function to update score
-    function updateScore() {
-
-    }
-    //Function for letter press
-    function letterPressO() {
-
-    }
-
-
 // MAIN PROCCESS
 
     document.onkeyup = function(event) {
-        function startGame()
 
-        if (letter === randLetter) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        console.log(computerGuess)
+
+
+
+        if (userGuess === computerGuess) {
+            alert("You win! Click okay to restart.")
+            updateLetter();
+            resetGame();
             amountWin++;
         } 
         else {
             amountLives--;
         }
         if (amountLives === 0) {
-            amountLoss++
+            alert("You lost! Click okay to restart.")
+            updateLetter();
+            resetGame();
+            amountLoss++;
+
         }
+        var html =
+        "<p>You chose: " + userGuess + "</p>" +
+        "<p>The computer chose: " + computerGuess + "</p>" +
+        "<p>wins: " + amountWin + "</p>" +
+        "<p>losses: " + amountLoss + "</p>" +
+        //"<p>Guesses so far: " + guessLetter "</p>"
+        "<p>lives: " + amountLives + "</p>";
+
+      // Set the inner HTML contents of the #game div to our html string
+        document.querySelector("#game").innerHTML = html;
     };
     
     
